593e02efb5cafc414a3a8edea0ff336c3849f214
yannickferire <yannick.ferire@gmail.com>
2023-06-08 02:30:16 +0200

    Best Units: Add loading feature

--- src/components/tome-of-traits/bestUnits.tsx
+++ src/components/tome-of-traits/bestUnits.tsx
@@ -3,9 +3,10 @@ import { validEmblems } from '@/constants/tome-of-traits';
 
 interface IBestUnits {
   champs: any[];
+  isLoading: boolean;
 }
 
-const BestUnits: React.FC<IBestUnits> = ({ champs }) => {
+const BestUnits: React.FC<IBestUnits> = ({ champs, isLoading }) => {
   const bestUnits = champs.filter((champion) => {
     const invalidTraits = champion.traits.filter((trait: string) => !validEmblems.some(emblem => emblem.name === trait));
     const hasValidTrait = invalidTraits.length === 0;
@@ -34,29 +35,46 @@ const BestUnits: React.FC<IBestUnits> = ({ champs }) => {
     return 0;
   });
 
+  const skeletonNumberOfBestUnits = Array.from({ length: 12 }, (_, index) => index + 1);
+
   return (
     <article className="pb-4 ml-4">
       <h3 className="opacity-50 mb-3 text-sm">Best units to use</h3>
-      <ul className="flex gap-2">
-      {sortedBestUnits.map((champion, index) => (
-        <li 
-          key={index} 
-          className="relative"
-          title={champion.name}
-        >
-          <div className={`border-2 border-${champion.cost}cost w-12 h-12 relative block rounded overflow-hidden`}>
-          <Image 
-            className={`w-20 -left-7 -top-1 max-w-none absolute z-10`}
-            src={champion.image} 
-            alt={champion.name} 
-            width="53" 
-            height="53" 
-          />
-          </div>
-          <div className={`absolute -top-1 left-1/2 -translate-x-1/2 z-20 rounded-sm text-earlynight bg-${champion.tier} flex w-4 h-4 items-center justify-center text-sm font-semibold uppercase`}><span>{champion.tier}</span></div>
-        </li>
-      ))}
-      </ul>
+      {isLoading === false ? (
+        <ul className="flex gap-2">
+        {sortedBestUnits.map((champion, index) => (
+          <li 
+            key={index} 
+            className="relative"
+            title={champion.name}
+          >
+            <div className={`border-2 border-${champion.cost}cost w-12 h-12 relative block rounded overflow-hidden`}>
+            <Image 
+              className={`w-20 -left-7 -top-1 max-w-none absolute z-10`}
+              src={champion.image} 
+              alt={champion.name} 
+              width="53" 
+              height="53" 
+            />
+            </div>
+            <div className={`absolute -top-1 left-1/2 -translate-x-1/2 z-20 rounded-sm text-earlynight bg-${champion.tier} flex w-4 h-4 items-center justify-center text-sm font-semibold uppercase`}><span>{champion.tier}</span></div>
+          </li>
+        ))}
+        </ul>
+      ) : (
+        <ul className="flex gap-2">
+          {skeletonNumberOfBestUnits.map((index) => (
+            <li 
+              key={index} 
+              role="status"
+              className={`animate-pulse w-12 h-12 flex-1`}
+            >
+              <div className="flex items-center justify-center aspect-square mx-auto bg-1cost rounded">
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </article>
   )
 }
