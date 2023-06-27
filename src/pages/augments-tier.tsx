790dc07df4d484680c96b30eb0c7be704c035bb7
yannickferire <yannick.ferire@gmail.com>
2023-06-27 18:37:20 +0200

    Augments Tier, fix meta tag

--- src/pages/augments-tier.tsx
+++ src/pages/augments-tier.tsx
@@ -17,17 +17,17 @@ const HeroAugments: NextPage = () => {
     <>
       <Head>
         <title>Augments tier possibilities – TFT odds Set {currentSet}{setStage === 2 ? '.5': null}</title>
-        <link rel="canonical" href="https://tftodds.com/hero-augments" />
-        <meta name="description" content="Discover how you can have the best chance to hit the champion augment your team needs! Find out the probability to hit every hero augments. Optimize your gameplay and climb the ranks with ease." />
-        <meta property="og:title" content="Hero Augments probabilities – TFT odds" />
-		    <meta property="og:description" content="Discover how you can have the best chance to hit the champion augment your team needs! Find out the probability to hit every hero augments. Optimize your gameplay and climb the ranks with ease." />
+        <link rel="canonical" href="https://tftodds.com/augments-tier" />
+        <meta name="description" content="Discover the probabilities of obtaining Silver, Gold, or Prismatic augments in your games with our Augments Tier Possibilities tool. Prepare yourself for the best augments your team needs!" />
+        <meta property="og:title" content="Augments tier possibilities – TFT odds Set 9" />
+		    <meta property="og:description" content="Discover the probabilities of obtaining Silver, Gold, or Prismatic augments in your games with our Augments Tier Possibilities tool. Prepare yourself for the best augments your team needs!" />
         <meta property="og:image" content="https://tftodds.com/share.jpg" />
-        <meta property="og:url" content="https://tftodds.com/hero-augments" />
+        <meta property="og:url" content="https://tftodds.com/augments-tier" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
 		    <meta name="twitter:site" content="@tftodds" />
-        <meta name="twitter:title" content="Hero Augments probabilities – TFT odds" />
-        <meta name="twitter:description" content="Discover how you can have the best chance to hit the champion augment your team needs! Find out the probability to hit every hero augments. Optimize your gameplay and climb the ranks with ease." />
+        <meta name="twitter:title" content="Augments tier possibilities – TFT odds Set 9" />
+        <meta name="twitter:description" content="Discover the probabilities of obtaining Silver, Gold, or Prismatic augments in your games with our Augments Tier Possibilities tool. Prepare yourself for the best augments your team needs!" />
         <meta name="twitter:image" content="https://tftodds.com/share.jpg" />
       </Head>
       <h1 className="text-3xl mt-4 mb-12 font-bold px-4 text-center"><strong className="text-morning">Augments tier</strong> possibilities</h1>
