57bb82e24d8a41155e5080b848fc8c0ce2c936ae
yannickferire <yannick.ferire@gmail.com>
2023-07-04 02:43:10 +0200

    Add Pandora's Bench odds

--- src/pages/data/augments-tables.tsx
+++ src/pages/data/augments-tables.tsx
@@ -2,6 +2,7 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { currentSet, setStage } from '@/constants/set';
 import { goldenEgg, spoilsOfWarSilver, spoilsOfWarSilverGold, spoilsOfWarGold, spoilsOfWarGoldGold, spoilsOfWarPrismatic, spoilsOfWarPrismaticGold, marchOfProgress } from "@/constants/augments";
+import { numberOfChampionsByCost } from "@/constants/champions";
 import { formatConsumables } from "@/utils/formatConsumables";
 import {
   Table,
@@ -52,7 +53,32 @@ const Piltover: NextPage = () => {
           })}
         </TableBody>
       </Table>
-      <h2 className="text-3xl mt-4 mb-2 font-bold px-4 text-center"><strong className="text-morning">March of Progress</strong></h2>
+      <h2 className="text-3xl mt-4 mb-2 font-bold px-4 text-center"><strong className="text-morning">Pandora&apos;s Bench</strong></h2>
+      <p className="text-center mb-4 max-w-lg relative left-1/2 -translate-x-1/2">Gain 2 gold. At the start of every round, champions on the 3 rightmost bench slots transform into random champions of the same cost.</p>
+      <Table className="mb-8">
+        <TableHeader>
+          <TableRow className="!border-b !border-crema !border-opacity-20">
+            <TableHead className="text-left"><span className="mr-2 bg-silver text-midnight/[.8] px-1 rounded-sm">Silver</span> Cost <span className="hidden md:inline">of champion you put in</span></TableHead>
+            <TableHead><span className="hidden md:inline">Specific champ (</span>1 slot<span className="hidden md:inline">)</span></TableHead>
+            <TableHead><span className="hidden md:inline">Specific champ (</span>2 slots<span className="hidden md:inline">)</span></TableHead>
+            <TableHead><span className="hidden md:inline">Specific champ (</span>3 slots<span className="hidden md:inline">)</span></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {Object.entries(numberOfChampionsByCost).map((champion, index) => {
+            const uniqueKey = `pandoras-bench-${index + 1}`;
+            return (
+              <TableRow key={uniqueKey}>
+                <TableCell className={`py-1 text-${champion[0].replace(' ', '')} border border-crema border-opacity-20`}>{champion[0]}</TableCell>
+                <TableCell className="py-1 font-semibold text-center text-base md:text-lg border-b border-r border-crema border-opacity-20">{((1/champion[1])*100).toFixed(2)}%</TableCell>
+                <TableCell className="py-1 font-semibold text-center text-base md:text-lg border-b border-r border-crema border-opacity-20">{((2/champion[1])*100).toFixed(2)}%</TableCell>
+                <TableCell className="py-1 font-semibold text-center text-base md:text-lg border-b border-r border-crema border-opacity-20">{((3/champion[1])*100).toFixed(2)}%</TableCell>
+              </TableRow>
+            );  
+          })}
+        </TableBody>
+      </Table>
+      <h2 className="pt-8 text-3xl mt-4 mb-2 font-bold px-4 text-center"><strong className="text-morning">March of Progress</strong></h2>
       <p className="text-center mb-4 max-w-lg relative left-1/2 -translate-x-1/2">Gain 3 XP now, and bonus XP equal to your level at the start of every player combat round. You can no longer use gold to level up.</p>
       <Table className="mb-8">
         <TableHeader>
