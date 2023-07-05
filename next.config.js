421611c08cd4edd42d9c5b2acc7bebd2844988b5
yannickferire <yannick.ferire@gmail.com>
2023-07-05 13:27:59 +0200

    Add compression in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	poweredByHeader: false,
+	compress: true,
 	images: {
 		domains: ["raw.communitydragon.org"],
 	},
