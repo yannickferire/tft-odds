559a1f3162e4971921d6d21c4ec5f16ebe98ad3f
yannickferire <yannick.ferire@gmail.com>
2023-08-02 13:44:36 +0200

    Piltover reworked

--- src/constants/piltover.tsx
+++ src/constants/piltover.tsx
@@ -1,12 +1,12 @@
 export const piltoverRewards: Record<string, {generic: string;} & Record<number, { value: string; percent: number }>> = {
-  "1-2 Energy;": {
+  "1 Energy;(0 loss)": {
     "generic": "1 Gold",
     1: {
       value: "1 Gold",
       percent: 100
     }
   },
-  "3-5 Energy;(1 loss)": {
+  "2 Energy;(1 loss)": {
     "generic": "2 Gold",
     1: {
       value: "2 Gold",
@@ -17,7 +17,7 @@ export const piltoverRewards: Record<string, {generic: string;} & Record<number,
       percent: 50
     }
   },
-  "6-8 Energy;(2 loss)": {
+  "4 Energy;(2 loss)": {
     "generic": "5 Gold",
     1: {
       value: "5 Gold",
@@ -28,11 +28,11 @@ export const piltoverRewards: Record<string, {generic: string;} & Record<number,
       percent: 25
     },
     3: {
-      value: "1x 2 cost + 1 Gold",
+      value: "2x 2 cost + 1 Gold",
       percent: 25
     }
   },
-  "9-12 Energy;(3 loss)": {
+  "6 Energy;(3 loss)": {
     "generic": "10 Gold",
     1: {
       value: "1x Item Component + 2 Gold",
@@ -47,7 +47,7 @@ export const piltoverRewards: Record<string, {generic: string;} & Record<number,
       percent: 25
     }
   },
-  "13-17 Energy;(4 loss)": {
+  "9 Energy;(4 loss)": {
     "generic": "15 Gold",
     1: {
       value: "1x Item Component + 7 Gold",
@@ -58,18 +58,18 @@ export const piltoverRewards: Record<string, {generic: string;} & Record<number,
       percent: 30
     },
     3: {
-      value: "3x 3 cost + 4 Gold",
+      value: "4x 3 cost + 3 Gold",
       percent: 30
     }
   },
-  "18-23 Energy;(5 loss)": {
+  "12 Energy;(5 loss)": {
     "generic": "22 Gold",
     1: {
       value: "2x Item Components + 6 Gold",
       percent: 40
     },
     2: {
-      value: "1x Tome of Traits + 3 Gold",
+      value: "1x Tome of Traits + 4 Gold",
       percent: 30
     },
     3: {
@@ -77,7 +77,7 @@ export const piltoverRewards: Record<string, {generic: string;} & Record<number,
       percent: 30
     }
   },
-  "24-29 Energy;(6 loss)": {
+  "16 Energy;(6 loss)": {
     "generic": "30 Gold",
     1: {
       value: "1x Completed Item Anvil + 15 Gold",
@@ -88,7 +88,7 @@ export const piltoverRewards: Record<string, {generic: string;} & Record<number,
       percent: 25
     },
     3: {
-      value: "1x Ornn Anvil + 12 Gold",
+      value: "1x Ornn Anvil + 15 Gold",
       percent: 25
     },
     4: {
@@ -96,52 +96,48 @@ export const piltoverRewards: Record<string, {generic: string;} & Record<number,
       percent: 20
     }
   },
-  "30-36 Energy;(7 loss)": {
+  "20 Energy;(7 loss)": {
     "generic": "42 Gold",
     1: {
       value: "2x Completed Anvil + 10 Gold",
-      percent: 30
+      percent: 35
     },
     2: {
-      value: "1x Tome of Traits + 22 Gold",
+      value: "5x Item Component",
       percent: 25
     },
     3: {
-      value: "1x Champion Duplicator + 28 Gold",
+      value: "1x Champion Duplicator + 30 Gold",
       percent: 20
     },
     4: {
-      value: "5x Item Component + 4 Gold",
-      percent: 20
-    },
-    5: {
       value: "1x Radiant Item + 2x Reforger + 12 Gold",
-      percent: 15
+      percent: 20
     }
   },
-  "37-44 Energy;(8 loss)": {
+  "25 Energy;(8 loss)": {
     "generic": "55 Gold",
     1: {
       value: "2x Ornn Anvil + 19 Gold",
       percent: 30
     },
     2: {
-      value: "1x Tactician's Crown + 24 Gold",
+      value: "1x Tactician's Crown + 25 Gold",
       percent: 25
     },
     3: {
-      value: "1x Radiant Item + 2x Reforger + 24 Gold",
+      value: "1x Radiant Item + 2x Reforger + 25 Gold",
       percent: 25
     },
     4: {
-      value: "1x 1 cost + 1x 2 cost + 1x 3 cost + 1x 4 cost + 1x 5 cost + 29 Gold",
+      value: "3x 5 cost + 40 Gold",
       percent: 20
     }
   },
-  "45-51 Energy;(9 loss)": {
+  "30 Energy;(9 loss)": {
     "generic": "70 Gold",
     1: {
-      value: "1x Tactician's Crown + 1x 4 cost + 20 Gold",
+      value: "1x Tactician's Crown + 4x 4 cost + 24 Gold",
       percent: 25
     },
     2: {
@@ -149,7 +145,7 @@ export const piltoverRewards: Record<string, {generic: string;} & Record<number,
       percent: 20
     },
     3: {
-      value: "4x Completed Anvil + 9 Gold",
+      value: "4x Completed Anvil + 10 Gold",
       percent: 20
     },
     4: {
@@ -157,34 +153,30 @@ export const piltoverRewards: Record<string, {generic: string;} & Record<number,
       percent: 20
     },
     5: {
-      value: "2x Spatula + 5 Item Components + 5 Gold",
+      value: "2x Spatula + 6 Item Components",
       percent: 15
     }
   },
-  "52-59 Energy;(10 loss)": {
+  "40 Energy;(10 loss)": {
     "generic": "85 Gold",
     1: {
-      value: "8x Item Component + 20 Gold",
+      value: "2x Radiant Item + 2x Reforger + 2x Removers + 25 Gold",
       percent: 30
     },
     2: {
-      value: "2x Radiant Item + 2x Reforger + 2x Removers + 25 Gold",
-      percent: 20
+      value: "1x Tactician's Crown + 1x Ornn Anvil + 1x Completed Anvil + 20 Gold",
+      percent: 30
     },
     3: {
-      value: "1x Tactician's Crown + 1x Ornn Anvil + 1x Completed Anvil + 20 Gold",
-      percent: 20
+      value: "1x Radiant Thief's Gloves + 1x Tactician's Crown + 3x 5 cost",
+      percent: 25
     },
     4: {
-      value: "1x Radiant Thief's Gloves + 1x Tactician's Crown + 3x 5 cost",
-      percent: 20
-    },
-    5: {
-      value: "1x Champion Duplicator + 75 Gold",
-      percent: 10
+      value: "1x Champion Duplicator + 1x Radiant Item + 1x Reforger + 1x Remover + 45 Gold",
+      percent: 15
     }
   },
-  "60-74 Energy;(11 loss)": {
+  "50 Energy;(11 loss)": {
     "generic": "110 Gold",
     1: {
       value: "1x Tactician's Crown + 80 Gold",
@@ -203,60 +195,56 @@ export const piltoverRewards: Record<string, {generic: string;} & Record<number,
       percent: 20
     }
   },
-  "75-89 Energy;(12 loss)": {
-    "generic": "140 Gold",
+  "60 Energy;(12 loss)": {
+    "generic": "135 Gold",
     1: {
-      value: "3x Radiant Item + 50 Gold",
+      value: "3x Radiant Item + 45 Gold",
       percent: 25
     },
     2: {
-      value: "1x Radiant Item + 1x Tactician's Crown + 1x Ornn Anvil + 1x Remover + 1x Reforger + 60 Gold",
+      value: "1x Radiant Item + 1x Tactician's Crown + 1x Ornn Anvil + 1x Remover + 1x Reforger + 55 Gold",
       percent: 25
     },
     3: {
       value: "Convert all regular items to Radiant + dynamic Gold",
-      percent: 20
+      percent: 25
     },
     4: {
-      value: "3x Spatula + 100 Gold",
+      value: "4x Spatula + 75 Gold",
       percent: 20
-    },
-    5: {
-      value: "1 of every double item",
-      percent: 10
     }
   },
-  "90-104 Energy;(13 loss)": {
-    "generic": "190 Gold",
+  "70 Energy;(13 loss)": {
+    "generic": "160 Gold",
     1: {
-      value: "2x Tactician's Crown + 2x Champion Duplicator + 2x Radiant Items + 1x Reforger + 30 Gold",
+      value: "2x Tactician's Crown + 2x Champion Duplicator + 2x Radiant Items + 1x Reforger + 15 Gold",
       percent: 30
     },
     2: {
-      value: "2x Radiant Items + 2x Reforger + 1x Tactician's Crown + 90 Gold",
-      percent: 30
+      value: "2x Radiant Items + 2x Reforger + 1x Tactician's Crown + 75 Gold",
+      percent: 25
     },
     3: {
-      value: "6x Completed Anvils + 1x Tactician's Crown + 50 Gold",
-      percent: 30
+      value: "3x Completed Anvils + 1x Radiant item + 1 Reforger + 1x Tactician's Crown + 50 Gold",
+      percent: 20
     },
     4: {
       value: "Convert all regular items to Radiant + dynamic Gold",
-      percent: 10
+      percent: 15
     },
   },
-  "105+ Energy;(14 loss)": {
-    "generic": "250 Gold",
+  "80 Energy;(14 loss)": {
+    "generic": "200 Gold",
     1: {
-      value: "3x Tactician's Crown + 3x Radiant Items + 2x Removers + 1x Reforger + 50 Gold",
+      value: "3x Tactician's Crown + 3x Radiant Items + 2x Removers + 1x Reforger + 25 Gold",
       percent: 35
     },
     2: {
-      value: "2x Champion Duplicator + 200 Gold",
+      value: "2x Champion Duplicator + 175 Gold",
       percent: 30
     },
     3: {
-      value: "2x Ornn Anvils + 2x Tactician's Crown + 2x Radiant Items + 2x Removers + 1x Tome of Traits + 50 Gold",
+      value: "3x Ornn Anvils + 2x Tactician's Crown + 2x Radiant Items + 2x Removers + 1x Tome of Traits + 25 Gold",
       percent: 30
     },
     4: {
@@ -268,12 +256,11 @@ export const piltoverRewards: Record<string, {generic: string;} & Record<number,
 
 export const sixPiltoversRewards = {
   "3 Gold": 30,
-  "1x 3 cost": 20,
-  "2 Gold": 10,
-  "1 Gold": 10,
-  "1x 1 cost + 1x 2 cost": 10,
-  "1x Item Component": 8,
-  "2x 2 cost": 7,
+  "1x 3 cost": 30,
+  "1x 2 cost + 1x 3 cost": 10,
+  "1x Item Component": 10,
+  "2x 2 cost": 10,
+  "1x 4 cost": 5,
   "1x Spatula": 4,
   "3x Remover": 1
 }
