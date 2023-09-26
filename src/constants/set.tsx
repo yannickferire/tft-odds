89be6f981f38b5eedf19cc14259b7c8f7558530d
yannickferire <yannick.ferire@gmail.com>
2023-09-26 15:19:38 +0200

    Update patch + rank Master

--- src/constants/set.tsx
+++ src/constants/set.tsx
@@ -2,7 +2,7 @@
 export const stateSet: string = "latest";
 export const currentSet: number = 9;
 export const setStage: number = 2;
-export const patch: string = "13.18";
+export const patch: string = "13.18b";
 export const apiURL = `https://raw.communitydragon.org/${stateSet}`;
 export const fetchChampionsURL = apiURL + "/cdragon/tft/en_us.json";
 export const championImageURL = apiURL + "/game/assets/ux/tft/championsplashes";
