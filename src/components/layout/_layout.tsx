6160b321df8d4a71122c01cc5a3ff2aa50f4afbf
Yannick Ferire <yannick@bureau347.com>
2024-12-04 01:26:21 +0100

    Space for ads on side

--- src/components/layout/_layout.tsx
+++ src/components/layout/_layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <>
-      <div className="w-full xl:container px-5 pt-6 sm:pt-10 pb-6 sm:pb-8 flex flex-col min-h-screen relative z-10">
+      <div className="w-full xl:container px-5 lg:px-32 pt-6 sm:pt-10 pb-6 sm:pb-8 flex flex-col min-h-screen relative z-10">
         <Header />
           {children}
         <Footer />
