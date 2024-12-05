e8472de73eafb7ec85751872a1072c3af50f62a9
Yannick Ferire <yannick@bureau347.com>
2024-12-05 11:59:03 +0100

    Add redirections

--- next.config.js
+++ next.config.js
@@ -6,6 +6,30 @@ const nextConfig = {
 	images: {
 		domains: ["raw.communitydragon.org", "pbs.twimg.com"],
 	},
+	async redirects() {
+		return [
+			{
+				source: "/augments-tier",
+				destination: "/augments/augments-tier",
+				permanent: true,
+			},
+			{
+				source: "/data/augments-distribution",
+				destination: "/augments/augments-distribution",
+				permanent: true,
+			},
+			{
+				source: "/data/augments-tables",
+				destination: "/augments/augments-tables",
+				permanent: true,
+			},
+			{
+				source: "/data/encounters",
+				destination: "/encounters",
+				permanent: true,
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
