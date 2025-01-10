b2a1037af3d8956b5debd14c90db5fae1d6c3c3c
Yannick Ferire <yannick@bureau347.com>
2025-01-10 13:56:04 +0100

    Patch 13.3

--- src/constants/set.tsx
+++ src/constants/set.tsx
@@ -2,7 +2,7 @@
 export const stateSet: string = "latest";
 export const currentSet: number = 13;
 export const setStage: number = 1;
-export const patch: string = "15.1";
+export const patch: string = "13.3";
 export const apiURL = `https://raw.communitydragon.org/${stateSet}`;
 export const fetchChampionsURL = apiURL + "/cdragon/tft/en_us.json";
 export const championImageURL = apiURL + "/game/assets/ux/tft/championsplashes/patching";
