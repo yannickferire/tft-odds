29c621ae78078131578b27c0cc317f05716535a4
Yannick Ferire <yannick@bureau347.com>
2025-12-14 23:58:04 +0100

    refactor: move champion constants from TSX to TS file.

--- src/constants/champions.ts
+++ src/constants/champions.ts
@@ -28,3 +28,41 @@ export const championUrlSquareMappings: Record<string, string> = {
 };
 
 export const excludedTraits = ["Deadeye", "Yordle", "Redeemer", "Shadow Isles"];
+
+export const numberOfChampionsByCost: { [cost: string]: number } = {
+  "1 cost": 14,
+  "2 cost": 13,
+  "3 cost": 13,
+  "4 cost": 12,
+  "5 cost": 8,
+};
+
+export const totalNumberOfChampions = Object.values(numberOfChampionsByCost)
+  .reduce((accumulator, currentValue) => accumulator + currentValue);
+
+export const numberOfUnlockableChampionsByCost: { [cost: string]: number } = {
+  "1 cost": 0,
+  "2 cost": 6,
+  "3 cost": 5,
+  "4 cost": 13,
+  "5 cost": 16,
+  "6 cost": 0,
+  "7 cost": 0
+};
+
+export const numberOfCopiesByCost: { [cost: string]: number } = {
+  "1 cost": 30,
+  "2 cost": 25,
+  "3 cost": 18,
+  "4 cost": 10,
+  "5 cost": 9,
+  "6 cost": 9,
+  "7 cost": 9,
+};
+
+export const numberOfCopiesForTier: { [tier: string]: number } = {
+  "1 star": 1,
+  "2 star": 3,
+  "3 star": 9,
+  "4 star": 27
+};
